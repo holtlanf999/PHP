cbb4eb5c0fa508f88d208737e75a09d2a1179ed7
Lanford Gabriel Murillo <holtlanf999@hotmail.com>
2015-06-19 16:47:36 -0600

    agregados estilos y pagina de thanks

--- Contact_Form/checkReg.php
+++ Contact_Form/checkReg.php
@@ -1,9 +1,21 @@
-<?php 
+<?php
 
-	if(empty($_POST['titulo'])){
-		echo 'titulo vacio';
-	} else {
-		echo 'titulo lleno';
+	class Template{
+		public function printThanks(){
+			if( empty( $_POST['titulo'] ) or empty( $_POST['email'] ) or empty( $_POST['message'] ) ){
+				echo '<p>Ha ocurrido un error</p>';
+			} else {
+				echo '<p>Gracias por registrarte!</p>';
+				echo '<p>El titulo de tu mensaje es: '.$_POST[ 'titulo' ].'</p>';
+				echo '<p>Tu correo es: '.$_POST[ 'email' ].'</p>';
+				echo '<p>Tu mensaje: '.$_POST[ 'message' ].'</p>'; 
+			}
+		}
+
 	}
+
+	$pt = new Template();
+
+	$pt->printThanks();
 	
 ?>
--- Contact_Form/index.php
+++ Contact_Form/index.php
@@ -1,67 +1,77 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Contact Form</title>
-		<link rel="stylesheet" href="css/main.css"/>
-		<meta charset="UTF-8"/>
-	</head>
-	<body>
+<?php 
 
-		<div id="contact-form">
+	class form{
 
-			<h1>Contact Form</h1>
+		public function initHtml(){
+			echo '<!DOCTYPE html>';
+			echo '<html>';
+		}
 
-			<?php 
+		public function createHead( $title, $charSet, $cssFileName ){
+			echo '<head>';
+				echo '<title>'.$title.'</title>';
+				echo '<meta charset="'.$charSet.'"/>';
+				echo '<link rel="stylesheet" href="css/'.$cssFileName.'">';
+			echo '</head>';
+		}
 
-				// $label;
-				// $labelVal;
-				// $name;
-				// $placeholder;
-				// $value;
+		public function initBody( $id, $title ){
+			echo '<body>';
+				echo '<div id="'.$id.'">';
+					echo '<h1>'.$title.'</h1>';
+		}
 
-				class form{
+		public function initForm(){
+			echo '<form id="register" action="checkReg.php" method="post" accept-charset="UTF-8">';
+		}
 
-					public function initForm(){
-						echo '<form id="register" action="checkReg.php" method="post" accept-charset="UTF-8">';
-					}
+		public function titulo( $label, $name, $placeholder ){
+			echo '<label for="'.$label.'" class="hidden">Titulo: </label>'.'<input type="text" name="'.$name.'" placeholder="'.$placeholder.'" required="required"/>';	
+		}
 
-					public function titulo( $label, $name, $placeholder ){
-						echo '<label for="'.$label.'" class="hidden">Titulo: </label>'.'<input type="text" name="'.$name.'" placeholder="'.$placeholder.'"/>';	
-					}
+		public function email( $label, $labelVal, $name, $placeholder ){
+			echo '<label for="'.$label.'" class="hidden">'.$labelVal.'</label>'.'<input type="email" name="'.$name.'" placeholder="'.$placeholder.'" required="required"/>';
+		}
 
-					public function email( $label, $labelVal, $name, $placeholder ){
-						echo '<label for="'.$label.'" class="hidden">'.$labelVal.'</label>'.'<input type="email" name="'.$name.'" placeholder="'.$placeholder.'"/>';
-					}
+		public function message( $label, $labelVal, $name, $placeholder ){
+			echo '<label for="'.$label.'" class="hidden">'.$labelVal.'</label>'.'<textarea name="'.$name.'" placeholder="'.$placeholder.'" required="required"></textarea>';	
+		}
 
-					public function message( $label, $labelVal, $name, $placeholder ){
-						echo '<label for="'.$label.'" class="hidden">'.$labelVal.'</label>'.'<textarea name="'.$name.'" placeholder="'.$placeholder.'"></textarea>';	
-					}
+		public function reset( $value ){
+			echo '<input type="reset" value="'.$value.'" class="button"/>';
+		}
 
-					public function reset( $value ){
-						echo '<input type="reset" value="'.$value.'" class="button"/>';
-					}
+		public function submit( $name ){
+			echo '<input type="submit" name="'.$name.'"  class="button"/>';
+		}
 
-					public function submit( $name ){
-						echo '<input type="submit" name="'.$name.'"  class="button"/>';
-					}
+		public function endForm(){
+			echo '</form>';
+		}
 
-					public function endForm(){
-						echo '</form>';
-					}
-				}
+		public function endBody(){
+				echo '</div>';
+			echo '</body>';
+		}
 
-				$printForm = new form();
+		public function endHtml(){
+			echo '</html>';
+		}
 
-				$printForm->initForm();
-				$printForm->titulo( 'Titulo', 'titulo', ' Título' );
-				$printForm->email( 'Email', 'email: ','email', ' email' );
-				$printForm->message( 'message', 'Mensaje: ', 'message',' Mensaje' );
-				$printForm->reset( 'Borrar datos del formulario' );
-				$printForm->submit( 'submit-btn' );
-				$printForm->endForm();
-			?>
+	}
 
-	</div><!-- contact-form -->
+	$printForm = new form();
 
-	</body>
-</html>
+	$printForm->initHtml();
+	$printForm->createHead( 'Contact Form', 'UTF-8', 'main.css' );
+	$printForm->initBody( 'contact-form', 'Contact Form' );
+	$printForm->initForm();
+	$printForm->titulo( 'Titulo', 'titulo', ' Título' );
+	$printForm->email( 'Email', 'email: ','email', ' email' );
+	$printForm->message( 'message', 'Mensaje: ', 'message',' Mensaje' );
+	$printForm->reset( 'Borrar datos del formulario' );
+	$printForm->submit( 'submit-btn' );
+	$printForm->endForm();
+	$printForm->endBody();
+	$printForm->endHtml();
+?>
